dbed668b8b08cd0bd3c7d5e5002e3bed31512b20
Aaron Ludwig <107589333+ahaaron05@users.noreply.github.com>
2024-12-22 00:57:43 -0700

    camera rotations added

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void process_input(GLFWwindow* window);
 void calculate_delta_time();
 
@@ -14,8 +15,9 @@ void calculate_delta_time();
 // Settings
 const unsigned int SCREEN_WIDTH = 900;
 const unsigned int SCREEN_HEIGHT = 700;
-const float FOV_Y = 100.0f;
+const float FOV_Y = 80.0f;
 const float DESIRED_CAMERA_SPEED = 10.0f;
+const float SENSITIVITY = 0.1f;
 
 float delta_time = 0.0f; // Time between current frame and last frame
 float last_frame_time = 0.0f;
@@ -23,6 +25,12 @@ float last_frame_time = 0.0f;
 glm::vec3 camera_pos = glm::vec3(0.0f, 0.0f, 5.0f);
 glm::vec3 camera_front = glm::vec3(0, 0, -1);
 glm::vec3 camera_up = glm::vec3(0, 1, 0);
+
+float last_x = SCREEN_WIDTH / 2;
+float last_y = SCREEN_HEIGHT / 2;
+float yaw = -90.0f;
+float pitch = 0.0f;
+bool first_mouse = true;
 
 int main()
 {
@@ -43,6 +51,8 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	// Init GLAD
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -130,8 +140,7 @@ int main()
 	glEnableVertexAttribArray(1);
 	glBindVertexArray(0); // unbind for safety
 	
-	
-	//view = glm::translate(view, glm::vec3(0.0f, 0.0f, -7.0f)); // NOTE: right-handed system
+
 	glm::mat4 projection(1.0f);
 	projection = glm::perspective(glm::radians(FOV_Y), (float)(SCREEN_WIDTH / SCREEN_HEIGHT), 0.1f, 100.0f); // NOTE: aspect ratio will determine FOV_X
 
@@ -197,6 +206,39 @@ int main()
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+}
+
+// mouse input related stuff
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if(first_mouse)
+	{
+		last_x = xpos;
+		last_y = ypos;
+		first_mouse = false;
+	}
+
+	float x_offset = xpos - last_x;
+	float y_offset = last_y - ypos; // reversed since y-coordinates range from bottom to top
+	last_x = xpos;
+	last_y = ypos;
+
+	x_offset *= SENSITIVITY;
+	y_offset *= SENSITIVITY;
+
+	yaw += x_offset;
+	pitch += y_offset;
+
+	if(pitch > 89.0f)
+		pitch = 89.0f;
+	if(pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 direction;
+	direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	direction.y = sin(glm::radians(pitch));
+	direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	camera_front = glm::normalize(direction);
 }
 
 // Handles all input within GLFW window
